91bb0752de5621cb55130d6759def8a07195a619
JeremyM02 <j3r3mee20>
2022-03-29 08:08:00 -0700

    Finished 6.4 Part Two way too early. Mispelled 6.3, but also fixed a missing req deleteArticle function.

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -81,7 +81,7 @@ module.exports.updateArticle = async function (req, res){
 
 module.exports.deleteArticle = async function(req, res){
     const article = await Article.findByPk(req.params.articleId);
-    if (!user.is('admin') && !article.isOwnedBy(req.user)){
+    if (!req.user.is('admin') && !article.isOwnedBy(req.user)){
         res.redirect('/');
         return;
     }
